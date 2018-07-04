cad1cd6dc6b1db8ef6082b7dc7b30b01105cddc5
ShanayaAnna <30575377+ShanayaAnna@users.noreply.github.com>
2018-07-04 11:37:47 +0530

    Rounded Edge Cuboid 2.js
    Another way to construct a cuboid with Rounded Edges
    Takes, as input: Width, Height, Depth, Radius, Smoothness
    
    src: https://discourse.threejs.org/t/round-edged-box/1402

---
+++ Rounded Edge Cuboid2.js
@@ -0,0 +1,23 @@
+function createBoxWithRoundedEdges( width, height, depth, radius0, smoothness ) {
+
+  let shape = new THREE.Shape();
+  let eps = 0.00001;
+  let radius = radius0 - eps;
+  shape.absarc( eps, eps, eps, -Math.PI / 2, -Math.PI, true );
+  shape.absarc( eps, height -  radius * 2, eps, Math.PI, Math.PI / 2, true );
+  shape.absarc( width - radius * 2, height -  radius * 2, eps, Math.PI / 2, 0, true );
+  shape.absarc( width - radius * 2, eps, eps, 0, -Math.PI / 2, true );
+  let geometry = new THREE.ExtrudeBufferGeometry( shape, {
+    amount: depth - radius0 * 2,
+    bevelEnabled: true,
+    bevelSegments: smoothness * 2,
+    steps: 1,
+    bevelSize: radius,
+    bevelThickness: radius0,
+    curveSegments: smoothness
+  });
+
+  geometry.center();
+
+  return geometry;
+}
